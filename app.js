4af3f5c31f14abd24bbb4ced0f9d30315c644d2e
AsHim Shrestha <130267320+AsHim1123@users.noreply.github.com>
2023-12-03 12:12:22 +0545

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const PORT = process.env.PORT;
 
 //middlewares
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
-// app.use(express.static(path.join(__dirname, "./frontend/build")));
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
-// });
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+});
 
 //routes
 readdirSync("./routes").map((route) => app.use("/api/v1", require("./routes/" + route)));
